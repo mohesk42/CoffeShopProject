d231be92ca8b89ac87ab999f6b540ae857a31324
Mohammad Muneer <mohammadesk42@gmail.com>
2021-07-17 12:41:38 +0300

    Update auth.py

--- backend/src/auth/auth.py
+++ backend/src/auth/auth.py
@@ -143,7 +143,7 @@ def verify_decode_jwt(token):
     raise AuthError({
                 'code': 'invalid_header',
                 'description': 'Unable to find the appropriate key.'
-            }, 400)
+            }, 403)
 
 '''
 @TODO implement @requires_auth(permission) decorator method
